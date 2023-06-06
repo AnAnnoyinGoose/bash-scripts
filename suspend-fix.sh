e270d1caa926b207ccb42e9e581d0afcb16f443f
AnAnnoyinGoose <anannoyingoose@gmail.com>
2023-06-06 20:50:04 +0200

    Push for ws sync

---
+++ suspend-fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+declare -a devices=(GPP0) 
+for device in "${devices[@]}"; do
+    if grep -qw ^$device.*enabled /proc/acpi/wakeup; then
+        sudo sh -c "echo $device > /proc/acpi/wakeup"
+    fi
+done
